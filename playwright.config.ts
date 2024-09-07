dd7f409ce4ae4fa94a93b2f845609cc166ed7587
QuangTo <tvquangcs@gmail.com>
2024-09-07 12:50:17 +0700

    update script and config

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    baseURL: process.env.URL,
+    baseURL: process.env.BASE_API,
     extraHTTPHeaders: {},
     trace: "on-first-retry",
     defaultBrowserType: "chromium",
