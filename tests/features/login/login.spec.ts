fc66be2151c3e1516279ebf9f87ed4b07db01e3e
QuangTo <tvquangcs@gmail.com>
2024-11-17 17:07:14 +0700

    added sample testcases

--- tests/features/login/login.spec.ts
+++ tests/features/login/login.spec.ts
@@ -1,15 +1,30 @@
 import { test, expect } from "@playwright/test";
 import { AuthService } from "../../../src/core/api/AuthService";
 
-test.describe("Authentication Tests", () => {
+test.describe("Authentication", { tag: "@smoke" }, () => {
   let authService: AuthService;
   test.beforeEach(async ({ request }) => {
     const baseUrl = "https://automationintesting.online";
     authService = new AuthService(request, baseUrl);
   });
-
-  test("User can login successfully", async () => {
+  test("Send POST with valid credential @happy", async () => {
     const response = await authService.login("admin", "password");
-    expect(response.status()).toEqual(200);
+    expect(response.status()).toBe(200);
+  });
+  test("Send POST with invalid username", async () => {
+    const response = await authService.login("admin1", "password");
+    expect(response.status()).toBe(403);
+  });
+  test("Send POST with invalid password", async () => {
+    const response = await authService.login("admin", "incorrectPassword");
+    expect(response.status()).toBe(403);
+  });
+  test("Send POST with invalid username& password", async () => {
+    const response = await authService.login("admin", "incorrectPassword");
+    expect(response.status()).toBe(403);
+  });
+  test("Send POST with no username no password", async () => {
+    const response = await authService.login("", "");
+    expect(response.status()).toBe(403);
   });
 });
