51d6ab843a48b692252e174f96bf2e48c2509959
QuangTo <tvquangcs@gmail.com>
2025-07-22 09:30:18 +0700

    test husky and eslint

--- src/api/endpoint/baseService.ts
+++ src/api/endpoint/baseService.ts
@@ -15,7 +15,7 @@ export class BaseService {
     this.headers = createHeaders();
   }
 
-  private async sendRequest(method: HttpMethod, url: string, body: { headers?: Record<string, string>; data?: Object } = {}): Promise<APIResponse> {
+  private async sendRequest(method: HttpMethod, url: string, body: { headers?: Record<string, string>; data?: object } = {}): Promise<APIResponse> {
     const fullHeaders = { ...this.headers, ...body.headers };
     const requestBody = {
       headers: fullHeaders,
@@ -38,18 +38,18 @@ export class BaseService {
     }
   }
 
-  async get(url: string, body: { headers?: Record<string, string>; data?: Object } = {}): Promise<APIResponse> {
+  async get(url: string, body: { headers?: Record<string, string>; data?: object } = {}): Promise<APIResponse> {
     return this.sendRequest('GET', url, body);
   }
 
-  async post(url: string, body: { headers?: Record<string, string>; data?: Object }): Promise<APIResponse> {
+  async post(url: string, body: { headers?: Record<string, string>; data?: object }): Promise<APIResponse> {
     return this.sendRequest('POST', url, body);
   }
-  async put(url: string, body: { headers?: Record<string, string>; data?: Object }): Promise<APIResponse> {
+  async put(url: string, body: { headers?: Record<string, string>; data?: object }): Promise<APIResponse> {
     return this.sendRequest('PUT', url, body);
   }
 
-  async delete(url: string, body: { headers?: Record<string, string>; data?: Object } = {}): Promise<APIResponse> {
+  async delete(url: string, body: { headers?: Record<string, string>; data?: object } = {}): Promise<APIResponse> {
     return this.sendRequest('DELETE', url, body);
   }
 
