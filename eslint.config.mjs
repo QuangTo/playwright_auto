e6eb4a7a62936d6ab52de176e436f836fe09af12
QuangTo <tvquangcs@gmail.com>
2025-07-22 09:30:41 +0700

    test husky and eslint2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ export default [
   { files: ['**/*.{ts}'] },
   eslint.configs.recommended,
   // ...playwright.configs['flat/recommended'],
-  ...tseslint.configs.recommendedTypeChecked,
+  // ...tseslint.configs.recommendedTypeChecked,
   {
     ignores: ['playwright-report/**/*', 'playwright.config.ts', 'node_modules/**', 'eslint.config.mjs']
   },
@@ -19,9 +19,9 @@ export default [
     },
     rules: {
       // ...playwright.configs['flat/recommended'].rules,
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       // 'eslint-disable no-empty-pattern': 'warn',
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
       'no-unused-expressions': 'warn',
       'no-constant-binary-expression': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
