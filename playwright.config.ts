3ac253daac3a7d05ffc22ed5e2c3cf40774d8c52
QuangTo <tvquangcs@gmail.com>
2025-10-25 14:05:20 +0700

    install report-portal docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
 import { loadEnvironment } from './load-env';
+import { defineConfig, devices } from '@playwright/test';
+// import { RPconfig } from 'src/setup/report/RPconfig';
 
 /**
  * Load the environment variables
@@ -21,9 +22,10 @@ export default defineConfig({
 
   /* Opt out of parallel tests on CI. */
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: [process.env.CI ? ['blob'] : ['list', { printSteps: true }], ['html', { noSnippets: true }]],
-  // reporter: process.env.CI ? 'blob' : 'html',
-  // ["@reportportal/agent-js-playwright", RPconfig],
+  // reporter: [process.env.CI ? ['blob'] : ['@reportportal/agent-js-playwright', RPconfig]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
